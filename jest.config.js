03fa76106d0ea15ff6652e85243312d3eca8d895
速水　達也 <t.hayamizu@outlook.com>
2020-04-01 16:04:08 +0900

    fix jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    // --------
+    // basic options
+    roots: ["__tests__"],
     globals: {
         "ts-jest": {
             tsConfig: "tsconfig.json",
@@ -7,6 +10,29 @@ module.exports = {
     transform: {
         "^.+\\.tsx?$": "ts-jest",
     },
-    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-    snapshotSerializers: ["enzyme-to-json/serializer"],
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+
+    verbose: true,
+
+    // ------
+    // module resolution
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleDirectories: ["./src", "./node_modules"],
+
+    modulePathIgnorePatterns: ["__data__", "__mock__"],
+
+    // --------
+    // coverage
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "**/*.{js,jsx,ts,tsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**",
+        "!**/build/**",
+        "!**/dist/**",
+        "!**/docs/**",
+        "!**/.vscode/**",
+        "!**/stories/**",
+    ],
+    coverageDirectory: "./dist/coverage",
 };
